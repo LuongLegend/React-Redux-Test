e19b74344196b75efcab86196dc97b7ba0e8d842
ninhluong <luongprolv99@gmail.com>
2021-11-19 22:25:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import { connect } from "react-redux";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react"; //
 
 import { getList, generateNewList } from "./actions/index";
 
 const listExample = ["goku", "bulma", "krillin", "yamcha", "broly"];
 
-function App({ list, onGetList, onGenerateNewList }) {
-  useEffect(() => {
-    onGetList(listExample);
+function App({ list, onGetList, onGenerateNewList }) {//props co dinh
+  //state thay doi. a = 1 -> a =2 a=3; react -> setA(2) ->a =2 -> cay DOM -> cap nhat
+  
+  const [a, setA] = useState(() => list);
+  
+  useEffect(() => { //lay du lieu ra. []
+    onGetList(listExample); //update Store
+    //khi lay duoc danh sach dia chi -> setA
   }, [onGetList]);
 
   const handleCheckBoxClick = (e) => {
@@ -21,14 +26,23 @@ function App({ list, onGetList, onGenerateNewList }) {
       onGetList(listExample);
     }
   };
+
+  const handleClickP = (aItem) => {
+    const aChange = list.filter(i => i !== aItem); //goku
+    setA(aChange)
+  }
   return (
     <div className="App">
       <h1>React Redux</h1>
       <input type="checkbox" onClick={handleCheckBoxClick} />
 
       {
-        list.map(i => <p key={i}>{i}</p>)
+        list.map(i => <p key={i} onClick={() => handleClickP(i)}>{i}</p>)
       }
+        <input type="checkbox" onClick={() => handleClickP('goku')} />
+        <input type="checkbox" onClick={handleClickP} />
+        <input type="checkbox" onClick={handleClickP} />
+      {a.toString()}
     </div>
   );
 }
